13b25fef9d566a06e10b06989d209c78402c7aa0
itsankk05 <aank.ag1234@gmail.com>
2023-08-21 14:36:09 +0530

    updates quiz

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import About from "./About/About";
 
+// import searchAnswer from "./Quiz/Function";
+
 function App() {
   // const [answer, setAnswer] = useState([]);
   const [quizs, setQuizs] = useState([]);
@@ -50,38 +52,119 @@ function App() {
 
   // Next Quesion
   const nextQuestion = () => {
-    storeAnswer(selectedAnswer);
     console.log(collectedAnswers);
+    console.log(collectedAnswers.length);
+    storeAnswer(selectedAnswer);
     setSelectedAnswer("");
     setQuestionIndex(questionIndex + 1);
     const rightBtn = document.querySelector("button.bg-success");
     rightBtn?.classList.remove("bg-success");
   };
 
-  const searchAnswer = () => {
-    const class12 = [];
-    const class10 = [
-      "Mathematics",
-      "Science",
-      "laboratory",
-      "reaerch",
-      "Analyzing",
-      "scientist",
-      "engineer",
-      "Mathematics",
-      "technological",
-      "logical reas",
+  function searchAnswer(collectedAnswers) {
+    const class10Sci = [
+      "Mathematics and Science",
+      "Solving puzzles or building things",
+      "Working in a laboratory or research setting",
+      "Analyzing data and solving problems",
+      "Becoming a scientist or engineer",
+      "Mathematics and technological advancements",
+      "Complex problem-solving and logical reasoning",
+      "Engineering and computer science",
+      "Innovating and contributing to scientific advancements",
+      '"Logic will get you from A to B. Imagination will take you everywhere." - Albert Einstein',
+      "Solving intricate puzzles and mathematical challenges",
+      "By analyzing and breaking down the components logically",
+      "Laboratories and hands-on experimentation",
+      "Solving complex problems and making discoveries",
+      "Making a positive impact on individuals and society",
+      "Flexible hours with opportunities for creativity",
+      "By seeking information and researching thoroughly",
+      "Structured and organized routine",
     ];
-    let class10Points = 0;
-    let class12Points = 0;
-    for (let i; i >= collectedAnswers.length; i++) {
-      if (collectedAnswers[i] in class12) {
-        class12Points = +1;
-      } else if (collectedAnswers[i] in class10) {
-        class10Points += 1;
+    const class10Commmerce = [
+      "Languages and Literature",
+      "Art and Creativity",
+      "Solving puzzles or building things",
+      "Participating in debates or discussions",
+      "Working in a quiet and focused environment",
+      "Working in a collaborative and social setting",
+      "Working in a visually creative environment",
+      "Connecting with people and understanding their perspectives",
+      "Pursuing a career in writing or journalism",
+      "Visual arts, design principles, and aesthetics",
+      "Transforming ideas into visually appealing creations",
+      "Literature and journalism",
+      "mathematical challenges",
+      "Fine arts and graphic design",
+      "Inspiring and influencing people through words",
+      "Creating visual designs and artistic projects",
+      "Flexible hours with opportunities for creativity",
+      "Structured and organized routine",
+      "Understanding diverse perspectives and helping others",
+      '"The pen is mightier than the sword." - Edward Bulwer-Lytton',
+    ];
+    const class10Arts = [
+      "Social Studies and History",
+      "Art and Creativity",
+      "Reading books or writing stories",
+      "Participating in debates or discussions",
+      "Drawing, painting, or crafting",
+      "Working in a quiet and focused environment",
+      "Working in a collaborative and social setting",
+      "Working in a visually creative environment",
+      "Expressing thoughts clearly in writing",
+      "Visualizing and creating artistic designs",
+      "Pursuing a career in writing or journalism",
+      "Working in a field related to human behavior or society",
+      "Exploring careers in the arts or design",
+      "History, cultures, and societal dynamics",
+      "Literature and journalism",
+      "Psychology and social work",
+      "Bringing positive changes to individuals and communities",
+      "Enriching lives through creative expressions",
+      "Interacting with people and understanding their emotions",
+      "By using your imagination and thinking creatively",
+      "Flexible hours with opportunities for creativity",
+      "Literature, language, and communication",
+      '"Art enables us to find ourselves and lose ourselves at the same time." - Thomas Merton',
+      '"Empathy is seeing with the eyes of another, listening with the ears of another, and feeling with the heart of another." - Alfred Adler',
+    ];
+
+    let class10PointsScience = 0;
+    let class10PointsCommerce = 0;
+    let class10PointsArts = 0;
+
+    for (let i = 0; i < collectedAnswers.length; i++) {
+      console.table(`Checking answer: ${collectedAnswers[i]}`);
+      if (class10Sci.includes(collectedAnswers[i])) {
+        console.table(`Found match for: ${collectedAnswers[i]}`);
+
+        class10PointsScience += 1;
       }
+      if (class10Commmerce.includes(collectedAnswers[i])) {
+        class10PointsCommerce += 1;
+        console.table(`Found match for: ${collectedAnswers[i]}`);
+      }
+      if (class10Arts.includes(collectedAnswers[i])) {
+        class10PointsArts += 1;
+        console.table(`Found match for: ${collectedAnswers[i]}`);
+      }
+      // Add similar logic for other categories if needed
     }
-  };
+
+    // Determine the ranking
+    const rankings = [
+      { category: "Science", points: class10PointsScience },
+      { category: "Commerce", points: class10PointsCommerce },
+      { category: "Arts", points: class10PointsArts },
+    ];
+
+    // rankings.sort((a, b) => b.points - a.points);
+
+    // Return the rankings
+    return rankings;
+  }
 
   const checkAnswer = (event, selected) => {
     if (!selectedAnswer) {
@@ -146,6 +229,8 @@ function App() {
             path="/result"
             element={
               <Result
+                collectedAnswers={collectedAnswers}
+                searchAnswer={searchAnswer}
                 showResult={showResult}
                 quizs={quizs}
                 // marks={marks}
--- src/Quiz/Result.js
+++ src/Quiz/Result.js
@@ -1,7 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Result = ({ showResult, quizs, marks, startOver }) => {
+function Result({
+  searchAnswer,
+  showResult,
+  quizs,
+  startOver,
+  collectedAnswers,
+}) {
+  if (!showResult) {
+    return null;
+  }
+
+  const rankings = searchAnswer(collectedAnswers);
+
+  //   return (
+  //     <div className="result-container">
+  //       <h2>Result</h2>
+  //       <div className="rankings">
+  //         <h3>Rankings:</h3>
+  //         <ul>
+  //           {rankings.map((rank, index) => (
+  //             <li key={index}>
+  //               {index + 1}. {rank.category}: {rank.points} points
+  //             </li>
+  //           ))}
+  //         </ul>
+  //       </div>
+  //       {/* Rest of your Result component */}
+  //     </div>
+  //   );
+  // }
+
   return (
     <section
       className="bg-dark text-white"
@@ -10,31 +40,30 @@ const Result = ({ showResult, quizs, marks, startOver }) => {
       <div className="container">
         <div className="row vh-100 align-items-center justify-content-center">
           <div className="col-lg-6">
-            <div
-              className={`text-light text-center p-5 rounded ${
-                marks > (quizs.length * 5) / 2 ? "bg-success" : "bg-danger"
-              }`}
-            >
-              <h1 className="mb-2 fw-bold">
-                Well Done! You Completed the Quiz
-              </h1>
-              {/* <h3 className="mb-3 fw-bold">
-                Your score is {marks} out of {quizs.length}
-              </h3> */}
-              <Link to="/start">
-                <button
-                  onClick={startOver}
-                  className="btn py-2 px-4 btn-light fw-bold d-inline"
-                >
-                  Start Over
-                </button>
-              </Link>
-            </div>
+            <h1 className="mb-2 fw-bold">
+              Seems Like You Are Interested in ....
+            </h1>
+            <ul style={{ listStyleType: "none" }}>
+              {rankings.map((rank, index) => (
+                <li key={index}>
+                  {index + 1}. {rank.category}:{" "}
+                  {((rank.points / 15) * 100).toFixed(2)} %
+                </li>
+              ))}
+            </ul>
+            <Link to="/form">
+              <button
+                onClick={startOver}
+                className="btn py-2 px-4 btn-light fw-bold d-inline"
+              >
+                Start Over
+              </button>
+            </Link>
           </div>
         </div>
       </div>
     </section>
   );
-};
+}
 
 export default Result;
