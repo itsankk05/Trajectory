5c840faec30f99eb769c2c6be97c4d670a98e738
itsankk05 <aank.ag1234@gmail.com>
2023-09-02 23:53:30 +0530

    json files

--- src/App.js
+++ src/App.js
@@ -22,31 +22,6 @@ function App() {
   const [showQuiz, setShowQuiz] = useState(false);
   const [showResult, setShowResult] = useState(false);
 
-  // Load JSON Data
-  // useEffect(() => {
-  //   fetch("quiz.json")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setQuizs(data);
-  //     });
-  // }, []);
-
-  // useEffect(() => {
-  //   setQuiz12Science(quiz12Science);
-  // }, [quiz12Science]);
-
-  // useEffect(() => {
-  //   fetch("quiz12Commerce.json")
-  //     .then((res) => res.json())
-  //     .then((data) => setQuiz12Commerce(data));
-  // }, []);
-
-  // useEffect(() => {
-  //   fetch("quiz12Arts.json")
-  //     .then((res) => res.json())
-  //     .then((data) => setQuiz12Arts(data));
-  // }, []);
-
   // Start Quiz
   const startQuiz = () => {
     setShowStart(false);
@@ -90,38 +65,41 @@ function App() {
     setShowQuiz(false);
   };
 
-  function searchAnswer(collectedAnswers) {
-    let class10PointsScience = 0;
-    let class10PointsCommerce = 0;
-    let class10PointsArts = 0;
+  function searchAnswer(collectedAnswers, selectedEducation) {
+    if (selectedEducation == 1) {
+      let class10PointsScience = 0;
+      let class10PointsCommerce = 0;
+      let class10PointsArts = 0;
 
-    for (let i = 0; i < collectedAnswers.length; i++) {
-      console.table(`Checking answer: ${collectedAnswers[i]}`);
-      if (class10Sci.includes(collectedAnswers[i])) {
-        console.table(`Found match for: ${collectedAnswers[i]}`);
+      for (let i = 0; i < collectedAnswers.length; i++) {
+        console.table(`Checking answer: ${collectedAnswers[i]}`);
+        if (class10Sci.includes(collectedAnswers[i])) {
+          console.table(`Found match for: ${collectedAnswers[i]}`);
 
-        class10PointsScience += 1;
+          class10PointsScience += 1;
+        }
+        if (class10Commerce.includes(collectedAnswers[i])) {
+          class10PointsCommerce += 1;
+          console.table(`Found match for: ${collectedAnswers[i]}`);
+        }
+        if (class10Arts.includes(collectedAnswers[i])) {
+          class10PointsArts += 1;
+          console.table(`Found match for: ${collectedAnswers[i]}`);
+        }
       }
-      if (class10Commerce.includes(collectedAnswers[i])) {
-        class10PointsCommerce += 1;
-        console.table(`Found match for: ${collectedAnswers[i]}`);
-      }
-      if (class10Arts.includes(collectedAnswers[i])) {
-        class10PointsArts += 1;
-        console.table(`Found match for: ${collectedAnswers[i]}`);
-      }
+
+      // Determine the ranking
+      const rankings = [
+        { category: "Science", points: class10PointsScience },
+        { category: "Commerce", points: class10PointsCommerce },
+        { category: "Arts", points: class10PointsArts },
+      ];
+
+      rankings.sort((a, b) => b.points - a.points);
+      // Return the rankings
+      return rankings;
+    } else if (selectedEducation == 2) {
     }
-
-    // Determine the ranking
-    const rankings = [
-      { category: "Science", points: class10PointsScience },
-      { category: "Commerce", points: class10PointsCommerce },
-      { category: "Arts", points: class10PointsArts },
-    ];
-
-    rankings.sort((a, b) => b.points - a.points);
-    // Return the rankings
-    return rankings;
   }
   return (
     <div className="app">
